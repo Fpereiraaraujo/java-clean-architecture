498bbad07a584b8c7379b6bfbfa81305c508295a
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2025-03-20 21:04:13 -0300

    feat:criando teste de fabrica de nascimento

--- src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -11,4 +11,13 @@ public class UsuarioTest {
         Assertions.assertThrows(IllegalAccessException.class,
         () -> new Usuario("123456789-99", "Jacque", LocalDate.parse("1990-09-08"), "email@email.com"));
     }
+
+    //Código omitido
+
+    @Test
+    public void deveCriarUsuarioUsandoFabricaDeUsuario(){
+        FabricaUsuario fabrica = new FabricaUsuario();
+        Usuario usuario = fabrica.comNomeCpfNacimento("Emily", "654.123.897-88",
+                LocalDate.parse("2000-10-01"));
+    }
 }
